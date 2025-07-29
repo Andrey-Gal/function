d42119ece2e6d6063139304ff2c0a83d23ad97c0
Andrey Galkin <andreygalkin85@gmail.com>
2025-07-30 00:11:39 +0300

    Добавлена функция calculate и вывод в консоль

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ const greetPavel = greeting('Hello', 'Pavel');
 // === Выводим результат в консоль (можно увидеть в терминале после запуска через node script.js)
 console.log(greetSergey); // → привет сергей
 console.log(greetPavel);  // → hello pavel
+
+function calculate(a, b, operation) {
+  if (operation === '+') {
+    return a + b;
+  } else if (operation === '-') {
+    return a - b;
+  } else if (operation === '*') {
+    return a * b;
+  } else if (operation === '/') {
+    return b !== 0 ? a / b : 'Ошибка: деление на 0';
+  } else {
+    return 'Неизвестная операция';
+  }
+}
+
+console.log(calculate(10, 5, '+')); // 15
+console.log(calculate(10, 5, '-')); // 5
+console.log(calculate(10, 5, '*')); // 50
+console.log(calculate(10, 0, '/')); // Ошибка: деление на 0
